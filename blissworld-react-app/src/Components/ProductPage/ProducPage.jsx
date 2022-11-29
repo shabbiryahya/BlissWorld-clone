55bf7cc8f740839103149857e48d995e9612ea06
Anurag <anuragn1107@gmail.com>
2022-11-29 15:08:21 +0530

    my second commit

--- blissworld-react-app/src/Components/ProductPage/ProducPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function ProducPage() {
-  return (
-    <div>ProducPage</div>
-  )
-}
-
-export default ProducPage
---
+++ blissworld-react-app/src/Components/ProductPage/ProductCard/ProductCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function ProductCard() {
+  return (
+    <div>
+        <div className="product-card">
+            <div className="product-image">
+                <img src="https://cdn11.bigcommerce.com/s-9p889rxpkb/images/stencil/1280x1280/products/300/1141/lemonsage_body_butter_sku_1003_10000_01__74065.1668812297.jpg?c=2" alt="product-image" />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ProductCard
