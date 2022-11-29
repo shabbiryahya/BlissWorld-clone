ff27719b5ffcaf3b0ab53f8e4b33ed50107a62d8
shabbir yahya <shabbirkatlariwala@gmail.com>
2022-11-29 22:50:04 +0530

    Minor Modified

--- blissworld-react-app/src/App.js
+++ blissworld-react-app/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import LandingPage from './Components/LandingPage/LandingPage';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <LandingPage/>
     </div>
   );
 }
--- blissworld-react-app/src/Components/LandingPage/LandingPage.jsx
+++ blissworld-react-app/src/Components/LandingPage/LandingPage.jsx
@@ -1,1 +1,13 @@
-import "../LandingPage/LandingPage.css"
+import '../LandingPage/LandingPage.css';
+
+import React from 'react';
+import Slider from './Slider/Slider';
+
+
+function LandingPage() {
+  return <div>
+    <Slider/>
+  </div>;
+}
+
+export default LandingPage;
