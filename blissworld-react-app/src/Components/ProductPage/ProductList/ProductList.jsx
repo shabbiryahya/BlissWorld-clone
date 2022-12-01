b84ba85d39038febba62efa873b391017d7f26b5
Anurag <anuragn1107@gmail.com>
2022-12-01 22:19:35 +0530

    sending the redux part for add to cart

--- blissworld-react-app/src/Components/ProductPage/ProductList/ProductList.jsx
+++ blissworld-react-app/src/Components/ProductPage/ProductList/ProductList.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ProductCard from "../ProductCard/ProductCard";
 import "./ProductList.css";
 import { ChevronDownIcon, MinusIcon, AddIcon } from "@chakra-ui/icons";
@@ -16,14 +16,34 @@ import {
   Box,
 } from "@chakra-ui/react";
 
-export const ProductList = () => {
+export const ProductList = ({ endPoint }) => {
+  const [totalProducts, setTotalProducts] = useState([]);
+  const [categories, setCategories] = useState({});
+
+  useEffect(() => {
+    fetch(`https://blissworldapi.onrender.com/bestSeller`)
+      .then((res) => res.json())
+      .then((data) => {
+        // console.log("total data", data);
+        setTotalProducts(data.totalData);
+        setCategories({ ...data.categories });
+      })
+      .catch((err) => {
+        console.log("something went wrong");
+      });
+  }, []);
+
+  console.log("state", categories);
+  // console.log("Object.keys(categories)", Object.keys(categories));
+  // console.log("Object.keys(categories)", categories["BY CONCERN"]);
+
   return (
     <div className="parent-container">
       <div className="sorting-div">
         <div className="sortBy">
-          <label>SORT BY:</label>
+          <label>SORT BY: Default</label>
           <Menu>
-            <MenuButton>Default {<ChevronDownIcon />}</MenuButton>
+            <MenuButton righticon={<ChevronDownIcon />}>D</MenuButton>
             <MenuList>
               <MenuItem>Default</MenuItem>
               <MenuItem>A to Z</MenuItem>
@@ -37,70 +57,38 @@ export const ProductList = () => {
       <div className="container">
         <div className="left-filter">
           <Accordion allowMultiple>
-          <AccordionItem>
-              {({ isExpanded }) => (
-                <>
-                  <h2>
-                    <AccordionButton>
-                      
-                      <Box flex="1" textAlign="left">
-                          BEST SELLERS BY CATEGORY
-
-                      </Box>
-                      {isExpanded ? (
-                        <MinusIcon fontSize="12px" />
-                      ) : (
-                        <AddIcon fontSize="12px" />
-                      )}
-                    </AccordionButton>
-                  </h2>
-                  <AccordionPanel textAlign="left" pb={2}>
-                    Body & Hair Care
-                  </AccordionPanel>
-                  <AccordionPanel textAlign="left" pb={2}>
-                    Skincare
-                  </AccordionPanel>
-                </>
-              )}
-            </AccordionItem>
-
-            <AccordionItem>
-              {({ isExpanded }) => (
-                <>
-                  <h2>
-                    <AccordionButton>
-                      <Box flex="1" textAlign="left">
-                        Section 2 title
-                      </Box>
-                      {isExpanded ? (
-                        <MinusIcon fontSize="12px" />
-                      ) : (
-                        <AddIcon fontSize="12px" />
-                      )}
-                    </AccordionButton>
-                  </h2>
-                  <AccordionPanel pb={4}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                  </AccordionPanel>
-                </>
-              )}
-            </AccordionItem>
+            {Object.keys(categories).length > 0 &&
+              Object.keys(categories).map((category, index) => (
+                <AccordionItem key={index + 1}>
+                  {({ isExpanded }) => (
+                    <>
+                      <h2>
+                        <AccordionButton>
+                          <Box flex="1" textAlign="left">
+                            {category}
+                          </Box>
+                          {isExpanded ? (
+                            <MinusIcon fontSize="12px" />
+                          ) : (
+                            <AddIcon fontSize="12px" />
+                          )}
+                        </AccordionButton>
+                      </h2>
+                      {categories[category].map((item, index) => (
+                        <AccordionPanel key={index + 1} textAlign="left" pb={2}>
+                          {item}
+                        </AccordionPanel>
+                      ))}
+                    </>
+                  )}
+                </AccordionItem>
+              ))}
           </Accordion>
         </div>
         <div className="product-container">
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
+          {totalProducts.map((item, index) => (
+            <ProductCard item={item} key={index + 1} />
+          ))}
         </div>
       </div>
     </div>
